c6a5ab96fba688ab0bde31ace1db861227ae8a1b
Graeme Winter <graeme.winter@gmail.com>
2023-01-01 20:00:16 +0000

    Quick go at using PIO

--- irq-timer/irq.c
+++ irq-timer/irq.c
@@ -6,6 +6,8 @@
 #include "hardware/timer.h"
 #include "pico/stdlib.h"
 
+#include "count.pio.h"
+
 // GPIO semaphore - arbitrary as not connected anywhere
 
 #define OUT 18
@@ -36,10 +38,15 @@ int main() {
   uint32_t mask = GPIO_IRQ_EDGE_RISE;
   gpio_set_irq_enabled_with_callback(IN, mask, true, &irq_callback);
 
+  uint32_t offset = pio_add_program(pio0, &count_program);
+  count_program_init(pio0, 0, offset_0, IN);
+  pio_sm_set_enabled(pio0, 0, true);
+
   while (true) {
     t0 = time_us_32();
     gpio_xor_mask(1 << OUT);
     sleep_ms(1000);
-    printf("%d\n", dt);
+    uint32_t count = 0xfffffffe - pio_sm_get_blocking(pio0, 0);
+    printf("%d %d\n", dt, count);
   }
 }
