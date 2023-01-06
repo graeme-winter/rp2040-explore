27bf11bab569f34e1c7c7c6e41fe71b86f71fc57
Graeme Winter <graeme.winter@gmail.com>
2023-01-06 12:04:54 +0000

    Remove zero

--- µPython-real-time/gpio_reg.py
+++ µPython-real-time/gpio_reg.py
@@ -7,23 +7,23 @@ import time
 led = 25
 
 # have to set the pin to SIO mode in the control register
-mem32[0x40014000 | 0x0CC] = 0x5
+mem32[0x40014000 | 0xCC] = 0x5
 
 # pad drive mode - 12mA
 mem32[0x4001C000 | 0x68] = 0x3 << 4
 
 # enable output
-mem32[0xD0000000 | 0x020] = 0x1 << led
+mem32[0xD0000000 | 0x20] = 0x1 << led
 
 
 def on():
     # set the right bit as on
-    mem32[0xD0000000 | 0x014] = 0x1 << led
+    mem32[0xD0000000 | 0x14] = 0x1 << led
 
 
 def off():
     # set the right bit as clear
-    mem32[0xD0000000 | 0x018] = 0x1 << led
+    mem32[0xD0000000 | 0x18] = 0x1 << led
 
 
 for j in range(10):
