702a1b3eb171b133b2676a38138add922c90b607
Graeme Winter <graeme.winter@gmail.com>
2023-01-13 21:57:41 +0000

    Simple UART readinto

--- µPython-real-time/uart.py
+++ µPython-real-time/uart.py
@@ -1,5 +1,4 @@
 from machine import UART, Pin
-import time
 
 uart0 = UART(0, baudrate=115200, tx=Pin(0), rx=Pin(1))
 uart1 = UART(1, baudrate=115200, tx=Pin(8), rx=Pin(9))
@@ -8,7 +7,11 @@ tx = bytearray(range(256))
 
 uart1.write(tx)
 
-rx = uart0.read()
+rx = bytearray(256)
+
+nn = uart0.readinto(rx)
+
+assert nn == 256
 
 for j in range(256):
     assert tx[j] == rx[j]
