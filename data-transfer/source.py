8f04d9d2a8b7ab009b7904ad8cb11a33c55da8d4
Graeme Winter <graeme.winter@gmail.com>
2023-01-26 20:29:44 +0000

    Viper / const

--- data-transfer/source.py
+++ data-transfer/source.py
@@ -6,31 +6,32 @@ import math
 from machine import Pin, mem32, UART
 from uctypes import addressof
 from rp2 import PIO, StateMachine, asm_pio
+import _thread
 
 # DREQ definitions
-DREQ_PIO0_TX0 = 0
+DREQ_PIO0_TX0 = const(0)
 
 # register definitions
-PIO0_BASE = 0x50200000
-PIO0_CTRL = PIO0_BASE + 0x00
-PIO0_FSTAT = PIO0_BASE + 0x04
-PIO0_TXF0 = PIO0_BASE + 0x10
+PIO0_BASE = const(0x50200000)
+PIO0_CTRL = const(PIO0_BASE + 0x00)
+PIO0_FSTAT = const(PIO0_BASE + 0x04)
+PIO0_TXF0 = const(PIO0_BASE + 0x10)
 
 # DMA registers
-DMA_BASE = 0x50000000
-CH0_READ_ADDR = DMA_BASE + 0x00
-CH0_WRITE_ADDR = DMA_BASE + 0x04
-CH0_TRANS_COUNT = DMA_BASE + 0x08
-CH0_CTRL_TRIG = DMA_BASE + 0x0C
-CH0_CTRL = DMA_BASE + 0x10
+DMA_BASE = const(0x50000000)
+CH0_READ_ADDR = const(DMA_BASE + 0x00)
+CH0_WRITE_ADDR = const(DMA_BASE + 0x04)
+CH0_TRANS_COUNT = const(DMA_BASE + 0x08)
+CH0_CTRL_TRIG = const(DMA_BASE + 0x0C)
+CH0_CTRL = const(DMA_BASE + 0x10)
 
-CH1_READ_ADDR = DMA_BASE + 0x40
-CH1_WRITE_ADDR = DMA_BASE + 0x44
-CH1_TRANS_COUNT = DMA_BASE + 0x48
-CH1_CTRL_TRIG = DMA_BASE + 0x4C
-CH1_CTRL = DMA_BASE + 0x50
+CH1_READ_ADDR = const(DMA_BASE + 0x40)
+CH1_WRITE_ADDR = const(DMA_BASE + 0x44)
+CH1_TRANS_COUNT = const(DMA_BASE + 0x48)
+CH1_CTRL_TRIG = const(DMA_BASE + 0x4C)
+CH1_CTRL = const(DMA_BASE + 0x50)
 
-MULTI_CHAN_TRIGGER = DMA_BASE + 0x430
+MULTI_CHAN_TRIGGER = const(DMA_BASE + 0x430)
 
 pins = [Pin(j) for j in range(8)]
 led = Pin(25, Pin.OUT)
@@ -59,13 +60,13 @@ def tick():
 
 
 # set up data: 1kHz sine wave
-COUNT = 100_000
+COUNT = const(125_000)
 data = bytearray(COUNT)
 
-for j in range(1000):
-    data[j] = int(128 + 127 * math.sin(0.001 * 2 * math.pi * j))
+for j in range(1250):
+    data[j] = int(128 + 127 * math.sin(0.0008 * 2 * math.pi * j))
     for k in range(1, 100):
-        data[j + 1000 * k] = data[j]
+        data[j + 1250 * k] = data[j]
 
 # FIXME run DMA in a second thread so the main thread could be used to
 # update the data array (say)
@@ -86,27 +87,34 @@ mem32[CH1_TRANS_COUNT] = COUNT // 4
 mem32[CH1_CTRL] = CTRL1
 
 # set up PIO
-sm0 = StateMachine(0, tick, freq=100_000, out_base=pins[0])
-
-# trigger DMA0 and PIO
-mem32[MULTI_CHAN_TRIGGER] = 1
-mem32[PIO0_CTRL] = 1
+sm0 = StateMachine(0, tick, freq=1_250_000, out_base=pins[0])
 
 BUSY = 1 << 24
 
-while True:
-    led.toggle()
-    while mem32[CH0_CTRL_TRIG] & BUSY:
-        continue
-    # set up CH0 again
-    mem32[CH0_READ_ADDR] = addressof(data)
-    mem32[CH0_WRITE_ADDR] = PIO0_TXF0
-    mem32[CH0_CTRL] = CTRL0
-    mem32[CH0_TRANS_COUNT] = COUNT // 4
-    while mem32[CH1_CTRL_TRIG] & BUSY:
-        continue
-    # set up CH1 again
-    mem32[CH1_READ_ADDR] = addressof(data)
-    mem32[CH1_WRITE_ADDR] = PIO0_TXF0
-    mem32[CH1_CTRL] = CTRL1
-    mem32[CH1_TRANS_COUNT] = COUNT // 4
+
+@micropython.viper
+def go():
+    # trigger DMA0 and PIO
+    mem32[MULTI_CHAN_TRIGGER] = 1
+    mem32[PIO0_CTRL] = 1
+
+    while True:
+        led.toggle()
+        while mem32[CH0_CTRL_TRIG] & BUSY:
+            continue
+        # set up CH0 again
+        mem32[CH0_READ_ADDR] = addressof(data)
+        mem32[CH0_WRITE_ADDR] = PIO0_TXF0
+        mem32[CH0_CTRL] = CTRL0
+        mem32[CH0_TRANS_COUNT] = COUNT // 4
+        led.toggle()
+        while mem32[CH1_CTRL_TRIG] & BUSY:
+            continue
+        # set up CH1 again
+        mem32[CH1_READ_ADDR] = addressof(data)
+        mem32[CH1_WRITE_ADDR] = PIO0_TXF0
+        mem32[CH1_CTRL] = CTRL1
+        mem32[CH1_TRANS_COUNT] = COUNT // 4
+
+
+_thread.start_new_thread(go, ())
