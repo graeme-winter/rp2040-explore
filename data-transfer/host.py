5689a392a97668b2af8564709296762be35c2c62
Graeme Winter <graeme.winter@gmail.com>
2023-01-27 08:21:33 +0000

    Plot start and end

--- data-transfer/host.py
+++ data-transfer/host.py
@@ -1,10 +1,22 @@
 import numpy
 import serial
+from matplotlib import pyplot
 
 tty = serial.Serial("/dev/tty.usbmodem14101", 115200 * 16)
 
-SIZE = 200_000
+SIZE = 100_000
 
 bytes = tty.read(SIZE)
 data = numpy.frombuffer(bytes, dtype=numpy.uint8).astype(numpy.uint32)
-print(numpy.sum(data))
+
+tty.close()
+
+x = numpy.array(range(1000))
+y = data.astype(numpy.uint8)
+
+y0 = y[1000:2000]
+y1 = y[-2000:-1000]
+
+pyplot.plot(x, y0)
+pyplot.plot(x, y1)
+pyplot.show()
