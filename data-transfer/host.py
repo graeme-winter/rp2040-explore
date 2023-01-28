cd42d27cffeea0891f603f69af3cda9d8dfdb3d0
Graeme Winter <graeme.winter@gmail.com>
2023-01-28 06:14:19 +0000

    Clean; average

--- data-transfer/host.py
+++ data-transfer/host.py
@@ -2,21 +2,18 @@ import numpy
 import serial
 from matplotlib import pyplot
 
+SIZE = 1_000_000
 tty = serial.Serial("/dev/tty.usbmodem14101", 115200 * 16)
-
-SIZE = 100_000
-
 bytes = tty.read(SIZE)
-data = numpy.frombuffer(bytes, dtype=numpy.uint8).astype(numpy.uint32)
-
 tty.close()
 
 x = numpy.array(range(1000))
-y = data.astype(numpy.uint8)
+y = numpy.frombuffer(bytes, dtype=numpy.uint8).astype(numpy.uint32)
 
-y0 = y[1000:2000]
-y1 = y[-2000:-1000]
+y0 = y[:1000]
+
+for k in range(1, 1000):
+    y0 += y[k * 1000 : (k + 1) * 1000]
 
 pyplot.plot(x, y0)
-pyplot.plot(x, y1)
 pyplot.show()
