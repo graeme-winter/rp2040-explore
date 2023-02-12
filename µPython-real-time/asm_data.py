d84ac32fdf31820dc76b4e4b7fc80aa694cb16f1
Graeme Winter <graeme.winter@gmail.com>
2023-02-12 05:53:48 +0000

    Clean, add timer

--- µPython-real-time/asm_data.py
+++ µPython-real-time/asm_data.py
@@ -1,4 +1,5 @@
 from machine import Pin
+import time
 
 led = Pin(25, Pin.OUT)
 pin = Pin(0, Pin.OUT)
@@ -14,8 +15,8 @@ def blink(r0):
     b(entry)
     align(4)
     data(4, (1 << 25) | 1)  # LED pin
-    data(4, 125)  # ticks
-    data(4, 10_000_000)  # cycles
+    data(4, 10)  # ticks
+    data(4, 100_000_000)  # total cycles
     align(2)
 
     label(entry)  # start of program
@@ -26,15 +27,18 @@ def blink(r0):
     ldr(r0, [r7, 0])
     str(r0, [r6, 0])  # toggle GPIO
     ldr(r0, [r7, 4])
-    sub(r0, r0, 2)  # two fewer counts to balance
     label(clock)
     sub(r0, r0, 1)
     cmp(r0, 0)
     bne(clock)
+    nop()
     nop()
     cmp(r1, 0)
     bne(tick)
     pop({r4, r5, r6, r7})
 
 
+t0 = time.ticks_us()
 blink(XOR)
+t1 = time.ticks_us()
+print(t1 - t0)
