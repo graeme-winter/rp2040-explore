9401a670de1be64d56ef447dcc6fa26b3733fb38
Graeme Winter <graeme.winter@gmail.com>
2023-02-28 05:31:32 +0000

    Address -> BASE; set GPIO0 also

--- asm/blink/blink.s
+++ asm/blink/blink.s
@@ -2,16 +2,18 @@
 .global main
 .align 2
 main:
-start:	
+start:
 	ldr r1, gpioc
 	mov r2, #5
-	str r2, [r1, #0]
+	str r2, [r1, #0x4]
+	str r2, [r1, #0xcc]
 
 	ldr r1, gpiod
 	mov r2, #48
-	str r2, [r1, #0]
+	str r2, [r1, #0x4]
+	str r2, [r1, #0x68]
 
-	ldr r1, gpio
+	ldr r1, sio
 	ldr r2, led
 	str r2, [r1, #0x20]
 loop:
@@ -24,8 +26,8 @@ tick:
 	b loop
 
 .align 4
-gpioc:	.word 0x400140cc
-gpiod:	.word 0x4001c068
-gpio:	.word 0xd0000000
-led:	.word 0x2000000
-count:	.word 5000000
+gpioc:	.word 0x40014000
+gpiod:	.word 0x4001c000
+sio:	.word 0xd0000000
+led:	.word 0x2000001
+count:	.word 100
