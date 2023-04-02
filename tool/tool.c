5c284c70daec0048bfeb2fa615ef6adc0d6a6a4a
Graeme Winter <graeme.winter@gmail.com>
2023-04-02 14:09:54 +0100

    ADC as unsigned short (well, uint12, really)

--- tool/tool.c
+++ tool/tool.c
@@ -18,7 +18,7 @@
 
 // data buffer
 #define SIZE 100000
-unsigned char data[SIZE];
+unsigned short data[SIZE];
 
 int main() {
   unsigned int counter = 0;
@@ -36,11 +36,11 @@ int main() {
   uart_set_hw_flow(uart0, false, false);
   uart_set_format(uart0, 8, 1, UART_PARITY_NONE);
 
-  // ADC configuration N.B. shifting to just keep 8 MSB
+  // ADC configuration
   adc_init();
   adc_gpio_init(ADC0);
   adc_select_input(0);
-  adc_fifo_setup(true, true, 1, false, true);
+  adc_fifo_setup(true, true, 1, false, false);
   adc_set_clkdiv(0);
 
   // ADC DMA configuration
@@ -48,7 +48,7 @@ int main() {
   dma_channel_config adc_dmac;
   adc_dma = dma_claim_unused_channel(true);
   adc_dmac = dma_channel_get_default_config(adc_dma);
-  channel_config_set_transfer_data_size(&adc_dmac, DMA_SIZE_8);
+  channel_config_set_transfer_data_size(&adc_dmac, DMA_SIZE_16);
   channel_config_set_dreq(&adc_dmac, DREQ_ADC);
   channel_config_set_read_increment(&adc_dmac, false);
   channel_config_set_write_increment(&adc_dmac, true);
@@ -66,8 +66,8 @@ int main() {
     dma_channel_wait_for_finish_blocking(adc_dma);
     adc_run(false);
 
-    printf("Sending %d bytes to UART @ %d\n", SIZE, baud);
-    uart_write_blocking(uart0, data, SIZE);
+    printf("Sending %d bytes to UART @ %d\n", SIZE * 2, baud);
+    uart_write_blocking(uart0, (unsigned char *) data, SIZE * 2);
     printf("Done\n");
     counter++;
   }
