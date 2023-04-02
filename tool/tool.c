bf8fec076a0d4523c71944abc1c7cf447cf41c99
Graeme Winter <graeme.winter@gmail.com>
2023-04-02 07:56:13 +0100

    Zeroth order tool

---
+++ tool/tool.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "pico.h"
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+#include "hardware/timer.h"
+
+int main()
+{
+    unsigned int counter = 0;
+
+    stdio_init_all();
+
+    while (true) {
+        printf("Counter: %d\n", counter);
+        counter++;
+        sleep_ms(1000);
+    }
+
+    return 0;
+}
