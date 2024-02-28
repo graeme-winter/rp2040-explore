db95365f80ff4d825258254d44725b79d50a205b
Graeme Winter <graeme.winter@gmail.com>
2024-02-28 05:44:16 +0000

    Typos; advance()

--- pico-panda/quad_generator.py
+++ pico-panda/quad_generator.py
@@ -1,5 +1,19 @@
-from machine import Pin
+from machine import Pin, mem32
 import rp2
+
+
+PIO0_BASE = 0x50200000
+PIO1_BASE = 0x50300000
+
+
+# advance one or more PIO SMs on PIO block pointed at by r0 masked by r1
+# call with e.g. advance(PIO1_BASE, 0x3) to advance SM 4, 5 by one tick
+@micropython.asm_thumb
+def advance(r0, r1):
+    mov(r2, 0x0)
+    str(r1, [r0, 0])
+    str(r2, [r0, 0])
+    mov(r0, 0)
 
 
 @rp2.asm_pio(set_init=(rp2.PIO.OUT_LOW, rp2.PIO.OUT_LOW))
@@ -36,10 +50,10 @@ def decr():
 
 # PIO1 blocks for signal generation
 
-pins = [machine.Pin(j, machine.Pin.OUT) for j in range(4)]
+pins = [Pin(j, Pin.OUT) for j in range(4)]
 
-sm4 = rp2.StateMachine(4, incr, freq=12_500_000, set_base=pins[0])
-sm5 = rp2.StateMachine(5, decr, freq=12_500_000, set_base=pins[0])
+sm4 = rp2.StateMachine(4, incr, freq=8_000_000, set_base=pins[0])
+sm5 = rp2.StateMachine(5, decr, freq=8_000_000, set_base=pins[0])
 
 
 @micropython.native
@@ -56,6 +70,14 @@ def updown(n, m):
 
 
 @micropython.native
+def up(m):
+    sm4.put((m // 4) - 1)
+    sm4.active(1)
+    sm4.get()
+    sm4.active(0)
+
+
+@micropython.native
 def down(m):
     sm5.put((m // 4) - 1)
     sm5.active(1)
@@ -63,5 +85,5 @@ def down(m):
     sm5.active(0)
 
 
-down(1_000)
-updown(10, 1_002_000)
+up(1000)
+down(1000)
